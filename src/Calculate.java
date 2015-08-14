12ca48de84307af13c633c57e5a66f097424528b
ivolova <ivolova2011@gmail.com>
2015-08-14 23:23:38 +0300

    Lesson_1_

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,10 +2,23 @@
  * Created by asusnote on 02.08.15.
  */
 public class Calculate {
-    public static void main(String[] args){
-        int first = Integer.valueOf(args[0]);
-        int second = Integer.valueOf(args[1]);
-        int sum = first + second;
-        System.out.println("Sum = "+ sum);
+
+    private int result = 0;
+    /** функция добавления элемента */
+    public void add(int[] params){
+        for (Integer param: params){
+            this.result =+ param;
+        }
     }
+    /**получить результат*/
+    public int getResult(){
+        return this.result;
+    }
+    /**очистить результат вычислений */
+    public void clearResult(){
+        this.result = 0;
+
+    }
+
+
 }
